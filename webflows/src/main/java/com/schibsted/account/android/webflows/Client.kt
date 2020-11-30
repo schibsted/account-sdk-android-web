86c0bbdfde153cf8def80679fcea36d65257ebe1
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2020-11-30 16:21:16 +0100

    Initial commit.

---
+++ webflows/src/main/java/com/schibsted/account/android/webflows/Client.kt
@@ -0,0 +1,7 @@
+package com.schibsted.account.android.webflows
+
+class Client {
+    fun helloWorld(): String {
+        return "Hello world!"
+    }
+}
---
+++ webflows/src/test/java/com/schibsted/account/android/webflows/ClientUnitTest.kt
@@ -0,0 +1,12 @@
+package com.schibsted.account.android.webflows
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ClientUnitTest {
+    @Test
+    fun helloWorld_isCorrect() {
+        assertEquals("Hello world!", Client().helloWorld())
+    }
+}
