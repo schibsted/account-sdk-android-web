8ecaed2af782a05f968181aff98d0b554ada3aec
astri-magnussen <astri.magnussen@schibsted.com>
2021-09-08 15:36:59 +0200

    Convert ExampleApp to kotlin

--- app/src/main/java/com/schibsted/account/example/ExampleApp.kt
+++ app/src/main/java/com/schibsted/account/example/ExampleApp.kt
@@ -1,31 +1,43 @@
-package com.schibsted.account.example;
+package com.schibsted.account.example
 
-import android.app.Application;
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.app.Application
+import android.app.PendingIntent
+import android.content.Intent
+import com.schibsted.account.example.ClientConfig.environment
+import com.schibsted.account.example.HttpClient.instance
+import com.schibsted.account.example.MainActivity.Companion.LOGIN_FAILED_EXTRA
+import com.schibsted.account.webflows.activities.AuthorizationManagementActivity
+import com.schibsted.account.webflows.client.ClientConfiguration
+import com.schibsted.account.webflows.client.Client
 
-import com.schibsted.account.webflows.activities.AuthorizationManagementActivity;
-import com.schibsted.account.webflows.client.Client;
-import com.schibsted.account.webflows.client.ClientConfiguration;
+class ExampleApp : Application() {
 
-public class ExampleApp extends Application {
-    public static Client client;
+    override fun onCreate() {
+        super.onCreate()
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+        initClient()
+        initAuthorizationManagement()
+    }
 
-        ClientConfiguration clientConfig = new ClientConfiguration(ClientConfig.getEnvironment(), ClientConfig.clientId, ClientConfig.loginRedirectUri);
-        client = new Client(getApplicationContext(), clientConfig, HttpClient.getInstance());
+    private fun initClient() {
+        val clientConfig =
+            ClientConfiguration(environment, ClientConfig.clientId, ClientConfig.loginRedirectUri)
+        client = Client(applicationContext, clientConfig, instance)
+    }
 
-        Intent completionIntent = new Intent(this, MainActivity.class);
-        Intent cancelIntent = new Intent(this, MainActivity.class);
-        cancelIntent.putExtra(MainActivity.Companion.getLOGIN_FAILED_EXTRA(), true);
-        cancelIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    private fun initAuthorizationManagement() {
+        val completionIntent = Intent(this, MainActivity::class.java)
+        val cancelIntent = Intent(this, MainActivity::class.java)
+        cancelIntent.putExtra(LOGIN_FAILED_EXTRA, true)
+        cancelIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         AuthorizationManagementActivity.setup(
-                client,
-                PendingIntent.getActivity(this, 0, completionIntent, 0),
-                PendingIntent.getActivity(this, 1, cancelIntent, 0)
-        );
+            client,
+            PendingIntent.getActivity(this, 0, completionIntent, 0),
+            PendingIntent.getActivity(this, 1, cancelIntent, 0)
+        )
+    }
+
+    companion object {
+        lateinit var client: Client
     }
 }
