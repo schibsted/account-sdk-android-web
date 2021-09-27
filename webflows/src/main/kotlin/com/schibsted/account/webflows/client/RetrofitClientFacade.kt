c034350c96b8a1aa00401da0632845dee556f49b
Anders Carlsen <anders.carlsen@schibsted.com>
2021-09-27 17:35:14 +0200

    Suggestion for adding Retrofit support to API with minimal impact on existing solution: * Only change to Client.kt is to make it override a ClientInterface * Added RetrofitClientFacade to wrap normal client, that add Retrofit service support * Retrofit service will be built when calling resumeLastLoggedInUser() RetrofitClientFacade * All other client calls to RetrofitClientFacade will by proxied to the normal Client instance * For Retrofit clients, makeAuthenticatedRequest() should be caused on RetrofitClientFacade instance and not on User object

---
+++ webflows/src/main/kotlin/com/schibsted/account/webflows/client/RetrofitClientFacade.kt
@@ -0,0 +1,55 @@
+package com.schibsted.account.webflows.client
+
+import com.schibsted.account.webflows.user.User
+import retrofit2.Retrofit
+
+/**  Represents a client registered with Schibsted account - with Retrofit support. */
+class RetrofitClientFacade<S>(
+    private val client: Client,
+    private val serviceClass: Class<S>,
+    private val retrofitBuilder: Retrofit.Builder,
+): ClientInterface by client {
+
+    private var retrofitApi: S? = null
+    private var user: User? = null
+
+    /** Resume any previously logged-in user session */
+    override fun resumeLastLoggedInUser(): User? {
+        val loggedInUser = client.resumeLastLoggedInUser()
+
+        if (loggedInUser != null) {
+            user = loggedInUser
+            retrofitApi = retrofitBuilder
+                .client(loggedInUser.httpClient)
+                .build()
+                .create(serviceClass)
+
+            return loggedInUser
+        }
+
+        user = null
+        retrofitApi = null
+        return null
+    }
+
+    /**
+     * Perform the given [request] with user access token as Bearer token in Authorization header.
+     *
+     * If the initial request fails with a 401, a refresh token request is made to get a new access
+     * token and the request will be retried with the new token if successful.
+     *
+     * If the refresh token request fails with an OAuth 'invalid_grant' error response, meaning the
+     * refresh token has expired or been invalidated, the user will be logged-out (and all existing
+     * tokens will be destroyed).
+     *
+     * @param request retrofit suspend request to perform with authentication using user tokens
+     */
+    suspend fun <V> makeAuthenticatedRequest(request: suspend (S) -> V?): V? {
+        if (user?.isLoggedIn() == true) {
+            retrofitApi?.let {
+                return request(it)
+            }
+        }
+        return null
+    }
+}
