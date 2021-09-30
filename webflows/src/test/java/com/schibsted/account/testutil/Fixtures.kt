8a4f40063eb63a9c9dba420c03b45c61c9318374
Anders Carlsen <anders.carlsen@schibsted.com>
2021-09-30 14:42:05 +0200

    Added some tests, update retrofit to support coroutines

--- webflows/src/test/java/com/schibsted/account/testutil/Fixtures.kt
+++ webflows/src/test/java/com/schibsted/account/testutil/Fixtures.kt
@@ -3,13 +3,17 @@ package com.schibsted.account.testutil
 import com.schibsted.account.webflows.api.SchibstedAccountApi
 import com.schibsted.account.webflows.client.Client
 import com.schibsted.account.webflows.client.ClientConfiguration
+import com.schibsted.account.webflows.client.RetrofitClientFacade
 import com.schibsted.account.webflows.persistence.SessionStorage
 import com.schibsted.account.webflows.persistence.StateStorage
 import com.schibsted.account.webflows.token.IdTokenClaims
 import com.schibsted.account.webflows.token.TokenHandler
 import com.schibsted.account.webflows.token.UserTokens
+import com.schibsted.account.webflows.util.TestRetrofitApi
 import io.mockk.mockk
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.net.URL
 
 internal object Fixtures {
@@ -43,6 +47,27 @@ internal object Fixtures {
             httpClient,
             tokenHandler,
             schibstedAccountApi
+        )
+    }
+
+    fun getRetrofitClient(
+        stateStorage: StateStorage = mockk(relaxed = true),
+        sessionStorage: SessionStorage = mockk(relaxed = true),
+        httpClient: OkHttpClient = Fixtures.httpClient,
+        tokenHandler: TokenHandler = mockk(relaxed = true),
+        schibstedAccountApi: SchibstedAccountApi = mockk(relaxed = true),
+        serviceClass: Class<TestRetrofitApi> = TestRetrofitApi::class.java,
+    ): RetrofitClientFacade<TestRetrofitApi> {
+        return RetrofitClientFacade(
+            internalClient = getClient(
+                stateStorage,
+                sessionStorage,
+                httpClient,
+                tokenHandler,
+                schibstedAccountApi
+            ),
+            serviceClass = serviceClass,
+            retrofitBuilder = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl("https://some.not.existing.domain")
         )
     }
 
