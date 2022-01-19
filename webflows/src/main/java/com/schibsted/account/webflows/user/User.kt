089d2ff1f5e832fd33500825f79697ddd01cc3b7
astri-magnussen <astri.magnussen@schibsted.com>
2022-01-19 09:17:34 +0100

    Public getter for idToken in User

--- webflows/src/main/java/com/schibsted/account/webflows/user/User.kt
+++ webflows/src/main/java/com/schibsted/account/webflows/user/User.kt
@@ -87,6 +87,11 @@ class User {
     }
 
     /**
+     * Get idToken for User
+     * */
+    fun getIdToken(): String? = tokens?.idToken
+
+    /**
      * Generate URL with embedded one-time code for creating a web session for the current user.
      *
      * @param clientId which client to get the code on behalf of, e.g. client id for associated web application
