18d5a2addf986519082f572e69d250857cf66f70
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2022-08-30 15:16:01 +0200

    Revert "Merge pull request #16 from schibsted/1681-make-usertokens-settable"
    This reverts commit c6530ff3ba3e8bb5f32cc9b462c6bb0ef09ab436, reversing
    changes made to 91eb93ac637d688946284e6a1819e09b7f52288a.

--- webflows/src/main/java/com/schibsted/account/webflows/token/IdTokenClaims.kt
+++ webflows/src/main/java/com/schibsted/account/webflows/token/IdTokenClaims.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class IdTokenClaims(
+internal data class IdTokenClaims(
     val iss: String,
     val sub: String,
     val userId: String,
