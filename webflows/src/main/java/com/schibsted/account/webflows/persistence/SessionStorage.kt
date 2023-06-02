0bcc02aac361411ecbcf1d7e5ad602e10ad448de
niculescu-bogdan-constantin <bogdan.niculescu@schibsted.com>
2023-06-02 16:38:50 +0200

    Clean shared prefs in case they are corrupted
    - update security-crypto fixing race condition

--- webflows/src/main/java/com/schibsted/account/webflows/persistence/SessionStorage.kt
+++ webflows/src/main/java/com/schibsted/account/webflows/persistence/SessionStorage.kt
@@ -2,6 +2,7 @@ package com.schibsted.account.webflows.persistence
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.google.gson.Gson
@@ -11,6 +12,7 @@ import com.schibsted.account.webflows.user.StoredUserSession
 import com.schibsted.account.webflows.util.Either
 import timber.log.Timber
 import java.security.GeneralSecurityException
+import java.security.KeyStore
 
 internal typealias StorageReadCallback = (Either<StorageError, StoredUserSession?>) -> Unit
 
@@ -28,27 +30,10 @@ internal interface SessionStorage {
 
 internal class EncryptedSharedPrefsStorage(context: Context) : SessionStorage {
     private val gson = GsonBuilder().setDateFormat("MM dd, yyyy HH:mm:ss").create()
+    private val appContext: Context = context.applicationContext
 
     private val prefs: SharedPreferences by lazy {
-        val masterKey = MasterKey.Builder(context.applicationContext)
-            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-            .build()
-
-        try {
-            EncryptedSharedPreferences.create(
-                context.applicationContext,
-                PREFERENCE_FILENAME,
-                masterKey,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-            )
-        } catch (e: GeneralSecurityException) {
-            Timber.e(
-                "Error occurred while trying to build encrypted shared preferences",
-                e
-            )
-            throw e
-        }
+        createEncryptedSharedPrefDestructively(context.applicationContext)
     }
 
     override fun save(session: StoredUserSession) {
@@ -80,15 +65,8 @@ internal class EncryptedSharedPrefsStorage(context: Context) : SessionStorage {
                 "Error occurred while trying to read from encrypted shared preferences",
                 e
             )
-            callback(Either.Left(StorageError.UnexpectedError(e)))
-        }
-    }
 
-    private fun Gson.getStoredUserSession(json: String): StoredUserSession? {
-        return try {
-            fromJson(json, StoredUserSession::class.java)
-        } catch (e: JsonSyntaxException) {
-            null
+            createEncryptedSharedPrefDestructively(this.appContext)
         }
     }
 
@@ -102,6 +80,70 @@ internal class EncryptedSharedPrefsStorage(context: Context) : SessionStorage {
                 "Error occurred while trying to delete from encrypted shared preferences",
                 e
             )
+        }
+    }
+
+    private fun createEncryptedSharedPrefDestructively(context: Context): SharedPreferences {
+        try {
+            return createEncryptedSharedPref(context)
+        } catch (e: GeneralSecurityException) {
+            Timber.e(
+                "Error occurred while trying to build encrypted shared preferences. Cleaning corrupted data",
+                e
+            )
+            deleteMasterKeyEntry()
+            deleteExistingPref(context)
+        }
+
+        try {
+            return createEncryptedSharedPref(context)
+        } catch (e: GeneralSecurityException) {
+            Timber.e(
+                "Encrypted shared preferences could not be created",
+                e
+            )
+
+            throw e
+        }
+    }
+
+    private fun deleteExistingPref(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            context.deleteSharedPreferences(PREFERENCE_FILENAME)
+        } else {
+            context.getSharedPreferences(PREFERENCE_FILENAME, Context.MODE_PRIVATE)
+                .edit()
+                .clear()
+                .commit()
+        }
+    }
+
+    private fun deleteMasterKeyEntry() {
+        KeyStore.getInstance("AndroidKeyStore").apply {
+            load(null)
+            deleteEntry(MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        }
+    }
+
+    private fun createEncryptedSharedPref(context: Context): SharedPreferences {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        return EncryptedSharedPreferences.create(
+            context,
+            PREFERENCE_FILENAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    private fun Gson.getStoredUserSession(json: String): StoredUserSession? {
+        return try {
+            fromJson(json, StoredUserSession::class.java)
+        } catch (e: JsonSyntaxException) {
+            null
         }
     }
 
