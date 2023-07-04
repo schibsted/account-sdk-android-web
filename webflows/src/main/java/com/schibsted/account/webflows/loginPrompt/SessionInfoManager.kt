7f8b1de1ca176922ef13af040115175142c78d7e
Wojciech Bakłażec <wojciech.baklazec@schibsted.com>
2023-07-04 13:23:50 +0200

    refactor: login prompt content provider refactor on xserxses comments

--- webflows/src/main/java/com/schibsted/account/webflows/loginPrompt/SessionInfoManager.kt
+++ webflows/src/main/java/com/schibsted/account/webflows/loginPrompt/SessionInfoManager.kt
@@ -3,15 +3,10 @@ package com.schibsted.account.webflows.loginPrompt
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import android.net.Uri
 import android.os.Build
-import com.schibsted.account.webflows.activities.RedirectUriReceiverActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
 
 
 class SessionInfoManager(context: Context) {
@@ -48,30 +43,26 @@ class SessionInfoManager(context: Context) {
     }
 
     suspend fun isUserLoggedInOnTheDevice(context: Context): Boolean {
-        return withContext(Dispatchers.IO) {
-            var contentProviders: List<ResolveInfo>;
-            val intent = Intent("com.schibsted.account.LOGIN_PROMPT_CONTENT_PROVIDER")
-            contentProviders = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                packageManager.queryIntentContentProviders(
-                    intent,
-                    PackageManager.ResolveInfoFlags.of(0)
-                )
-            } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                packageManager.queryIntentContentProviders(intent, PackageManager.MATCH_ALL)
-            }
-            else {
-                packageManager.queryIntentContentProviders(intent, 0)
-            }
-            val result = async {
-                for (contentProvider in contentProviders) {
-                    if (isSessionPresent(contentProvider.providerInfo.authority)) {
-                        return@async true
-                        break
-                    }
-                }
-                return@async false
-            }
-            result.await()
+        var contentProviders: List<ResolveInfo>;
+        val intent = Intent("com.schibsted.account.LOGIN_PROMPT_CONTENT_PROVIDER")
+        contentProviders = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> packageManager.queryIntentContentProviders(
+                intent,
+                PackageManager.ResolveInfoFlags.of(0)
+            )
+
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> packageManager.queryIntentContentProviders(
+                intent,
+                PackageManager.MATCH_ALL
+            )
+
+            else -> packageManager.queryIntentContentProviders(intent, 0)
         }
+        for (contentProvider in contentProviders) {
+            if (isSessionPresent(contentProvider.providerInfo.authority)) {
+                return true
+            }
+        }
+        return false
     }
 }
