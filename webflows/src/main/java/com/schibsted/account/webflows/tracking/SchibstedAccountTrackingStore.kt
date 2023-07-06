fd51f251735c8a8038e2213af58a6dae93a64585
filip-misztal <filip.misztal@schibsted.com>
2023-07-06 11:19:59 +0200

    Document API

--- webflows/src/main/java/com/schibsted/account/webflows/tracking/SchibstedAccountTrackingStore.kt
+++ webflows/src/main/java/com/schibsted/account/webflows/tracking/SchibstedAccountTrackingStore.kt
@@ -8,10 +8,18 @@ interface SchibstedAccountTrackerStore {
         private val accountListenersList: MutableList<SchibstedAccountTrackingListener> =
             mutableListOf()
 
+        /**
+         * Adds listener for tracking events
+         */
         override fun addTrackingListener(trackingListener: SchibstedAccountTrackingListener) {
             accountListenersList.add(trackingListener)
         }
 
+        /**
+         * Remove listener for tracking events
+         * This api might be useful for unit testing as mock listeners should be
+         * removed after all tests.
+         */
         override fun removeTrackingListener(trackingListener: SchibstedAccountTrackingListener) {
             accountListenersList.remove(trackingListener)
         }
