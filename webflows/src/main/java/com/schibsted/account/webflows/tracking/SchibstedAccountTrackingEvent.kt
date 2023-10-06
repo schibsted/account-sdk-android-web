44e722ffd608da54540414292bdd3ef9a7a099a7
niculescu-bogdan-constantin <bogdan.niculescu@schibsted.com>
2023-10-06 13:50:29 +0200

    add extra properties for events

--- webflows/src/main/java/com/schibsted/account/webflows/tracking/SchibstedAccountTrackingEvent.kt
+++ webflows/src/main/java/com/schibsted/account/webflows/tracking/SchibstedAccountTrackingEvent.kt
@@ -1,17 +1,35 @@
 package com.schibsted.account.webflows.tracking
 
+import com.schibsted.account.webflows.BuildConfig
+
+const val PACKAGE_NAME = "account-sdk-android-web"
+const val PROVIDER_COMPONENT = "schibsted-account"
+
 sealed class SchibstedAccountTrackingEvent {
+    val providerComponent = PROVIDER_COMPONENT
+    val deployTag = "${PACKAGE_NAME}-${BuildConfig.VERSION_NAME}"
+
     object LoginPromptCreated : SchibstedAccountTrackingEvent()
+
     object LoginPromptView : SchibstedAccountTrackingEvent()
+
     object LoginPromptLeave : SchibstedAccountTrackingEvent()
+
     object LoginPromptDestroyed : SchibstedAccountTrackingEvent()
+
     object LoginPromptClickToLogin : SchibstedAccountTrackingEvent()
+
     object LoginPromptClickToContinueWithoutLogin : SchibstedAccountTrackingEvent()
+
     object LoginPromptClickOutside : SchibstedAccountTrackingEvent()
+
     object LoginPromptContentProviderInsert : SchibstedAccountTrackingEvent()
+
     object LoginPromptContentProviderDelete : SchibstedAccountTrackingEvent()
 
     object UserLoginSuccessful : SchibstedAccountTrackingEvent()
+
     object UserLoginFailed : SchibstedAccountTrackingEvent()
+
     object UserLoginCanceled : SchibstedAccountTrackingEvent()
 }
